c1f77ddbda2a1acccf58a1091b59e83a24e22d92
dan-arki <100216647+dan-arki@users.noreply.github.com>
2024-06-05 10:29:32 +0200

    ee

--- front/pages/watchlist/index.vue
+++ front/pages/watchlist/index.vue
@@ -1,7 +1,19 @@
 <template>
   <UiHeader></UiHeader>
-  <section class="flex justify-start items-center h-screen mx-10">
+  <!-- <section class="flex justify-start items-center h-screen mx-10">
     <UiPostersList :titleText="'WatchList'"></UiPostersList>
+  </section> -->
+
+  <!-- v-for watchlist -->
+  <section v-if="watchlist.length > 0" class="mx-10">
+    <h1 class="text-3xl font-bold text-white">Watchlist</h1>
+    <div class="grid grid-cols-2 gap-4">
+      <UiPoster
+        v-for="media in watchlist"
+        :key="media.id"
+        :media="media"
+      ></UiPoster>
+    </div>
   </section>
 </template>
 
